a9b2f2becb4d0e45cd2a6d4786bae6548c26ac46
ttcat <ttcat0902@gmail.com>
2015-12-24 15:53:25 +0800

    put more files

---
+++ js.php
@@ -0,0 +1,10 @@
+<!-------------MENU效果------------>
+<link href="images/style0211.css" rel="stylesheet" type="text/css" />
+<link href="images/menu_effect.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="images/fancybox.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="images/js_slider.css" />
+
+<script type="text/javascript" src="https://s.yimg.com/ji/campaign/jquery/jquery_183.js"></script>
+<script type="text/javascript" src="images/menu_effect.js"></script>
+<script type="text/javascript" src="images/js_slider.js"></script>
+<script type="text/javascript" src="images/swfobject.js"></script>
